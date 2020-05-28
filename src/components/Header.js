319c7afa05fb033de8a7476656b80e1c9917ce51
Razielini <razielco@gmail.com>
2020-05-27 21:38:03 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import '../styles/components/Header.styl';
+import Logo from './Logo';
 
 const Header = () => {
   return (
     <header>
-      <div>LOGO</div>
-      <nav>MENU</nav>
+      <div className="Header-logo">
+        <Logo />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://github.com/Razielini">TECHNOLOGY</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">IDEAS</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">VIDEO</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">LEADERSHIP</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">NEWS</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">FINANCE</a>
+          </li>
+          <li>
+            <a href="https://github.com/Razielini">ENTERTAINMENT</a>
+          </li>
+        </ul>
+      </nav>
       <div>HAM</div>
     </header>
   );
