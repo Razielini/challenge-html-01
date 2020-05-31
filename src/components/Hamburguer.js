ba54feebb01e3f5c80de01eb291fa3ad97d21d02
Razielini <razielco@gmail.com>
2020-05-31 15:10:45 -0600

    Create Hamburguer.js

---
+++ src/components/Hamburguer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/components/Hamburguer.styl';
+
+const Hamburguer = () => {
+  return (
+    <div className="Hamburguer">
+      <div className="Hamburguer-element" />
+      <div className="Hamburguer-element" />
+      <div className="Hamburguer-element" />
+    </div>
+  );
+};
+
+export default Hamburguer;
